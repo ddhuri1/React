498c50e3ab402c0f307ccdab73beef469e5f071e
devina dhuri <ddhuri1@binghamton.edu>
2020-06-16 19:05:58 -0400

    Routing for data fom firebase

--- project-manager/src/components/projects/ProjectDetails.js
+++ project-manager/src/components/projects/ProjectDetails.js
@@ -1,21 +1,44 @@
-  import React from 'react'
+import React from 'react'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 
 const ProjectDetails = (props) => {
-  const id = props.match.params.id;
-  return (
-    <div className="container section project-details">
+  const {project} = props;
+  if(project) {
+    return (<div className="container section project-details">
       <div className="card z-depth-0">
         <div className="card-content">
-          <span className="card-title">Project title - { id }</span>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et labore quaerat quibusdam vel saepe, ab voluptate accusantium culpa nemo fuga earum? Soluta amet nobis officia sed neque fuga aperiam quia?</p>
-        </div>
+          <span className="card-title"> { project.title }</span>
+            <p>{ project.content }</p>
+          </div>
         <div className="card-action grey lighten-4 grey-text">
-          <div>Posted by The AUTHOR</div>
-          <div>2nd September, 2am</div>
+        <div>Read more on : {project.link}</div>
         </div>
       </div>
     </div>
-  )
+    )
+  }else{
+    return (
+      <div className="container center">
+        <p>Loading!!</p>
+      </div>
+    )
+  }
 }
 
-export default ProjectDetails
+const mapStateToProps = (state, ownProps) => {
+  const id = ownProps.match.params.id;
+  const projects = state.firestore.data.projects;
+  const project = projects ? projects[id] : null
+  return {
+    project : project
+  }
+}
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    { collection: 'projects' }
+  ])
+)(ProjectDetails)
--- project-manager/src/components/projects/ProjectList.js
+++ project-manager/src/components/projects/ProjectList.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import ProjectSummary from './ProjectSummary'
+import {Link} from 'react-router-dom';
 
 const ProjectList = ({projects}) => {
   return (
     <div className="project-list section"> 
       {projects && projects.map(project => {
         return(
-          <ProjectSummary project={project} key={project.id}/>
+          <Link to = {"/project/" + project.id}>
+          <ProjectSummary project={project} key={project.id}/></Link>
         )
       })}
     </div>
